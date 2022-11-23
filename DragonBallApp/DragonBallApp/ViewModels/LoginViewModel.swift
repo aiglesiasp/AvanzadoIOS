db3b533742314aa35b906fb5e8fc6aeeb233182a
aiglesiasp <iglesiaspubillaitor@gmail.com>
2022-11-23 15:22:14 +0100

    LoginViewModel
    Habia desaparecido

---
+++ DragonBallApp/DragonBallApp/ViewModels/LoginViewModel.swift
@@ -0,0 +1,55 @@
+//
+//  LoginViewModel.swift
+//  DragonBallApp
+//
+//  Created by Aitor Iglesias Pubill on 22/11/22.
+//
+
+import UIKit
+import KeychainSwift
+
+final class LoginViewModel {
+    private var networkModel: NetworkModel
+    private var keychain: KeychainSwift
+    
+    //Para comunicrme con la vista que hay un error
+    var onError: ((String) -> Void)?
+    var onLogin: (() -> Void)?
+    
+    //init
+    init(networkModel: NetworkModel = NetworkModel(),
+         keychain: KeychainSwift = KeychainSwift(),
+         onError: ((String) -> Void)? = nil,
+         onLogin: (() -> Void)? = nil)
+    {
+        self.networkModel = networkModel
+        self.keychain = keychain
+        self.onError = onError
+        self.onLogin = onLogin
+    }
+    
+    
+    //MARK: LOGIN
+    func login(with user: String, password: String) {
+        networkModel.login(user: user, password: password) { [weak self] token, error in
+            if let error = error {
+                self?.onError?(error.localizedDescription)
+            }
+            
+            //Compruebo que el token no esta vacio y son iguales
+            guard let token = token, !token.isEmpty else {
+                self?.onError?("Wrong token")
+                return
+            }
+            
+            self?.keychain.set(token, forKey: "KCToken")
+            self?.onLogin?()
+            
+            
+            
+            
+            
+        }
+    }
+    
+}
